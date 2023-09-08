48b9e33717f2c1faae601f2252f80057e81a8f5c
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-09-08 20:34:56 +0300

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import { headerLogo} from '../assets/images';
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav>
+        <a href="/">
+          <img 
+          src={headerLogo}
+           alt="Logo"
+           width={130}
+           height={30} />
+        </a>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import PopularProduct from './PopularProduct';
+import CustomerReview from './CustomerReview';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopularProduct,
+    CustomerReview,
+    Services,
+    SuperQuality,
+    SpecialOffer,
+    Subscribe,
+    Footer
+} 
+
