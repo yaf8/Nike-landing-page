6ea69ff0cad33266fa1ade2e671a60ee209aadee
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-09-09 03:48:55 +0300

    add all components and sections with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
-import { CustomerReview, Hero, Subscribe, SuperQuality, Services, Footer, PopularProduct, SpecialOffer} from './sections';
-import Nav from './components/Nav';
+import { Nav } from "./components";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 
 const App = () => {
   return (
-    <main className="relative">
+    <main className='relative'>
       <Nav />
-      <section className="xl:padding-l wide:padding-r padding-b">
+      <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
-      <section className="padding">
-        <PopularProduct />
+      <section className='padding'>
+        <PopularProducts />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SuperQuality />
       </section>
-      <section className="padding-x py-10">
+      <section className='padding-x py-10'>
         <Services />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SpecialOffer />
       </section>
-      <section className="bg-pale-blue padding">
-        <CustomerReview />
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
       </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
+      <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className="bg-black padding-x padding-t pb-8">
+      <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
 export default App;
--- src/sections/index.js
+++ src/sections/index.js
@@ -1,20 +1,19 @@
-import Hero from './Hero';
-import PopularProduct from './PopularProduct';
-import CustomerReview from './CustomerReview';
-import Services from './Services';
-import SpecialOffer from './SpecialOffer';
-import SuperQuality from './SuperQuality';
-import Subscribe from './Subscribe';
-import Footer from './Footer';
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
 
 export {
     Hero,
-    PopularProduct,
-    CustomerReview,
-    Services,
+    PopularProducts,
     SuperQuality,
+    Services,
     SpecialOffer,
+    CustomerReviews,
     Subscribe,
     Footer
-} 
-
+}
